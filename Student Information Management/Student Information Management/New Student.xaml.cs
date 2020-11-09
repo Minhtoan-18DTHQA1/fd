cb20df02aa92ff273eba3a179359c63aaa74b726
Minhtoan-18DTHQA1 <71840754+Minhtoan-18DTHQA1@users.noreply.github.com>
2020-11-10 00:18:48 +0700

    Add files via upload

---
+++ Student Information Management/Student Information Management/New Student.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Student_Information_Management
+{
+    /// <summary>
+    /// Interaction logic for New_Student.xaml
+    /// </summary>
+    public partial class New_Student : Window
+    {
+        public New_Student()
+        {
+            InitializeComponent();
+        }
+    }
+}
